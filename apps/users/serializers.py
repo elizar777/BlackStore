6799fb83cfdfaaf4a469e9cbf2b1a9dfc8b35ed6
elizar777 <ahmadbekovelizar390@gmail.com>
2024-04-29 16:52:54 +0600

    hide some info

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,4 +5,4 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'username','first_name', 'last_name', 'date_joined')
