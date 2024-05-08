35034ec41e2d5aa48e8a96524da6e226ffd3e7df
elizar777 <ahmadbekovelizar390@gmail.com>
2024-05-08 19:36:55 +0600

    safety is better 2

--- apps/products/permissions.py
+++ apps/products/permissions.py
@@ -2,4 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class ProductPermissions(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(obj.user.pk == request.user.pk)z
+        return bool(obj.user.pk == request.user.pk)
